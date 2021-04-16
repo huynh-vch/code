1257345f6a630c13eedb56a3c82381c12f70513d
huynh <20520015@gm.uit.edu.vn>
2021-04-16 16:27:43 +0700

    upload code bai 03

---
+++ 20520015/TH01/05-3/main.cpp
@@ -0,0 +1,12 @@
+#include"candidate.h"
+#include"testcandidate.h"
+
+int main()
+{
+	testcandidate KT;
+	KT.Nhap();
+	KT.Kiemtra();
+
+	system("pause");
+	return 1;
+}
---
+++ 20520015/TH01/05-3/testcandidate.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "candidate.h"
+#include <iostream>
+using namespace std;
+
+class testcandidate
+{
+private:
+	int SoTS;
+	candidate* DSTS;
+public:
+	void Nhap();
+	void Kiemtra();
+};
